78e028cfc230bf705050688eedf2a041177cc15c
ruslan.humeniuk <rus.gumeniuk@gmail.com>
2024-09-27 16:44:54 +0300

    feat: make project's technologies and responsibilities collapsible

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { JobProject } from '../../models/job';
 import Bubble from '../shared/bubble/Bubble';
 import Period from '../shared/period/Period';
@@ -9,12 +9,40 @@ interface ProjectProperties {
 }
 
 const Project: FC<ProjectProperties> = ({ project }) => {
+  const [isTechnologiesDisplayed, setIsTechnologiesDisplayed] = useState(false);
+  const [isResponsibilitiesDisplayed, setIsResponsibilitiesDisplayed] =
+    useState(false);
+
+  const changeTechnologiesSectionVisability = (): void => {
+    setIsTechnologiesDisplayed(!isTechnologiesDisplayed);
+  };
+
+  const changeResponsibilitiesSectionVisability = (): void => {
+    setIsResponsibilitiesDisplayed(!isResponsibilitiesDisplayed);
+  };
+
   return (
     <div className="project">
       <div className="project-head">
         <div className="project-title">
           <strong>Project "{project.title}"</strong>
         </div>
+        <button
+          className={`expander ${
+            isTechnologiesDisplayed ? 'enabled' : 'disabled'
+          }`}
+          onClick={changeTechnologiesSectionVisability}
+        >
+          <Bubble value="Technologies" />
+        </button>
+        <button
+          className={`expander ${
+            isResponsibilitiesDisplayed ? 'enabled' : 'disabled'
+          }`}
+          onClick={changeResponsibilitiesSectionVisability}
+        >
+          <Bubble value="Responsibilities" />
+        </button>
         <div className="project-period">
           <Period
             startedDate={project.startedOn}
@@ -23,20 +51,24 @@ const Project: FC<ProjectProperties> = ({ project }) => {
           />
         </div>
       </div>
-      <ul className="project-technologies">
-        {project.technologies.map((tech, i) => (
-          <li className="technology" key={i}>
-            <Bubble value={tech} />
-          </li>
-        ))}
-      </ul>
-      <ul className="responsibilities">
-        {project.responsibilities.map((resp, i) => (
-          <li className="text" key={i}>
-            {resp}
-          </li>
-        ))}
-      </ul>
+      {isTechnologiesDisplayed && (
+        <ul className="project-technologies">
+          {project.technologies.map((tech, i) => (
+            <li className="technology" key={i}>
+              <Bubble value={tech} />
+            </li>
+          ))}
+        </ul>
+      )}
+      {isResponsibilitiesDisplayed && (
+        <ul className="responsibilities">
+          {project.responsibilities.map((resp, i) => (
+            <li className="text" key={i}>
+              {resp}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
