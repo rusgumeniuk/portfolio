7719b9a434ebadd52f2e167014bb867edba90b7e
ruslan.humeniuk <rus.gumeniuk@gmail.com>
2024-10-03 20:14:11 +0300

    feat: use SkillList in Skills, add Skill interface

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,23 +1,36 @@
 import { FC } from 'react';
-import Bubble from '../shared/bubble/Bubble';
+import Divider from '../shared/divider/Divider';
+import SkillList from '../shared/skill-list/SkillList';
 import { SkillGroups } from './data';
 import './Skills.css';
+import styles from './Skills.module.css';
 
 const Skills: FC = () => {
   return (
-    <div className="skill-groups">
-      {SkillGroups.map((group) => (
-        <div className="skill-group" key={group.title}>
-          <span className="skill-group-title">{group.title}</span>
-          <ul className="skill-list">
-            {group.skills.map((skill) => (
-              <li className="skill" key={skill}>
-                <Bubble value={skill} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+    <div className="skills-container">
+      <h3 className="title">Skills</h3>
+      <Divider />
+      <div className="skill-groups">
+        {SkillGroups.map((group) => (
+          <div className="skill-group" key={group.title}>
+            <span className="skill-group-title">{group.title}</span>
+            <div className="skill-list-container">
+              {/* TODO: mark skills with non commerce experience in a different color */}
+              <SkillList
+                mainSkills={group.skills
+                  .filter(
+                    (x) => !x.levelOfUsage || x.levelOfUsage === 'Commercial'
+                  )
+                  .map((x) => x.title)}
+                additionalSkills={group.skills
+                  .filter((x) => x.levelOfUsage === 'NonCommercial')
+                  .map((x) => x.title)}
+                additionalSkillsClassName={styles.additional}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/models/skills.ts
+++ src/models/skills.ts
@@ -1,11 +1,19 @@
+export interface Skill {
+  title: string;
+  levelOfUsage?: 'NonCommercial' | 'Commercial';
+}
+
 export interface SkillGroup {
   title: string;
-  skills: string[];
+  skills: Skill[];
 }
 
 export const Technologies = {
   React: 'React',
-  Angular: 'Angular',
+  Angular: 'Angular 16',
+  Html: 'HTML5',
+  Css: 'CSS3',
+  Bootstrap: 'Bootstrap',
   AspNetApi: 'ASP.NET API',
   Redux: 'Redux',
   Typescript: 'TypeScript',
