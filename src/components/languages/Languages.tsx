1ec45b65893fed18e102ffaae3ad478d3080d614
ruslan.humeniuk <rus.gumeniuk@gmail.com>
2024-10-03 20:04:02 +0300

    feat: prettify Languages, add divider

--- src/components/languages/Languages.tsx
+++ src/components/languages/Languages.tsx
@@ -1,22 +1,27 @@
 import { FC } from 'react';
+import Divider from '../shared/divider/Divider';
 import { LanguagesData } from './data';
 import './Languages.css';
 
 const Languages: FC = () => {
   return (
     <div className="languages">
-      <h3>Languages</h3>
-      {Object.entries(LanguagesData).map(([title, level]) => (
-        <div className="language">
-          <span className="language-title">
-            <strong>{title}</strong>
-          </span>
-          <span>-</span>
-          <span className="language-level">
-            <strong>{level}</strong>
-          </span>
-        </div>
-      ))}
+      <h3 className="title">Languages</h3>
+      <Divider />
+      {/* TODO: create SectionTitle component? */}
+      <div className="languages-list">
+        {Object.entries(LanguagesData).map(([title, level]) => (
+          <div className="language" key={title}>
+            <span className="language-title">
+              <strong>{title}</strong>
+            </span>
+            <span>-</span>
+            <span className="language-level">
+              <strong>{level}</strong>
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
