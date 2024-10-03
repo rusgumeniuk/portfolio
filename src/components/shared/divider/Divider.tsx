b403390f4b6b64fce69eca2cce48c0647847bc79
ruslan.humeniuk <rus.gumeniuk@gmail.com>
2024-10-03 19:59:05 +0300

    feat: create Divider component

---
+++ src/components/shared/divider/Divider.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './Divider.css';
+
+const Divider: FC = () => {
+  return (
+    <div className="divider-container">
+      <div className="section-divider" />
+    </div>
+  );
+};
+
+export default Divider;
