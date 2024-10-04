03da1167227a232771d1bd19ff8434761446bea9
ruslan.humeniuk <rus.gumeniuk@gmail.com>
2024-10-04 19:42:30 +0300

    feat: use css module for SkillList, add hint for additional skills

--- src/components/shared/skill-list/SkillList.tsx
+++ src/components/shared/skill-list/SkillList.tsx
@@ -1,28 +1,38 @@
 import { FC } from 'react';
-import './SkillList.css';
+import styles from './SkillList.module.css';
 
 interface SkillListProps {
   mainSkills: string[];
   additionalSkills?: string[];
   additionalSkillsClassName?: string;
+  additionalSkillsNote?: string;
 }
 
 const SkillList: FC<SkillListProps> = ({
   mainSkills,
   additionalSkills,
   additionalSkillsClassName,
+  additionalSkillsNote,
 }) => {
   return (
-    <ul className="skill-list">
+    <ul className={styles['skill-list']}>
       {mainSkills.map((skill) => (
-        <li className="skill" key={skill}>
+        <li className={styles.skill} key={skill}>
           <span>{skill}</span>
         </li>
       ))}
       {additionalSkills &&
         additionalSkills.map((skill) => (
-          <li className={`skill ${additionalSkillsClassName}`} key={skill}>
+          <li
+            className={`${styles.skill} ${styles['additional-skill']} ${additionalSkillsClassName}`}
+            key={skill}
+          >
             <span>{skill}</span>
+            {additionalSkillsNote && (
+              <div className={styles.hint}>
+                <span>{additionalSkillsNote}</span>
+              </div>
+            )}
           </li>
         ))}
     </ul>
