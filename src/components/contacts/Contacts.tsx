3f32b206eaccca77af11085cbf47ca3b79d71569
ruslan.humeniuk <rus.gumeniuk@gmail.com>
2024-10-18 14:38:41 +0300

    feat: temporary disable linkedin link

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,4 +1,4 @@
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import {
   faEnvelope,
   faGlobe,
@@ -19,13 +19,14 @@ const Contacts: FC = () => {
           text="Email"
         />
       </div>
-      <div className="contact link">
+      {/* TODO: uncomment
+       <div className="contact link">
         <Icon
           iconTitle={faLinkedin}
           link={ContactsData.linkedIn}
           text="LinkedIn"
         />
-      </div>
+      </div> */}
       <div className="contact link">
         <Icon iconTitle={faGithub} link={ContactsData.github} text="GitHub" />
       </div>
